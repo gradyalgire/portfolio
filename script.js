c8dc64ee5836b0c4a1c7b2b5f31cfc18d2c6569e
gradyalgire <gdalgire04@gmail.com>
2025-04-17 19:56:58 -0400

    barebones portfolio

---
+++ script.js
@@ -0,0 +1,54 @@
+// Source: Lun Dev Code
+let cards = document.querySelectorAll('.projects .project-slider .project-card');
+let card_info = document.querySelectorAll('.projects .project-info');
+
+let active = 0;
+function loadShow(){
+    cards[active].style.transform = `none`;
+    cards[active].style.zIndex = 1;
+    cards[active].style.filter = 'none';
+    cards[active].style.opacity = 1;
+
+    card_info[active].style.transform = `none`;
+    card_info[active].style.zIndex = 1;
+    card_info[active].style.filter = 'none';
+    card_info[active].style.opacity = 1;
+    // show after
+    let stt = 0;
+    for(var i = active + 1; i < cards.length; i ++){
+        stt++;
+        cards[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px)`;
+        cards[i].style.zIndex = -stt;
+        cards[i].style.filter = 'blur(5px)';
+        cards[i].style.opacity = 0;
+
+        card_info[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px)`;
+        card_info[i].style.zIndex = -stt;  
+        card_info[i].style.filter = 'blur(5px)';
+        card_info[i].style.opacity = 0;
+    }
+     stt = 0;
+    for(var i = (active - 1); i >= 0; i --){
+        stt++;
+        cards[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px)`;
+        cards[i].style.zIndex = -stt;
+        cards[i].style.filter = 'blur(5px)';
+        cards[i].style.opacity = 0;
+
+        card_info[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px)`;
+        card_info[i].style.zIndex = -stt;
+        card_info[i].style.filter = 'blur(5px)';
+        card_info[i].style.opacity = 0;
+    }
+}
+loadShow();
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+next.onclick = function(){
+   active = active + 1 < cards.length ?  active + 1 : active;
+   loadShow();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active -1 : active;
+    loadShow();
+}
